51ee4192ed1bb0c47c0c9a4620cf3712e407b8ee
Marcin Rainka <marcinrainka@gmail.com>
2016-07-31 16:39:55 +0200

    Initial Commit

---
+++ iQuizek/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iQuizek
+//
+//  Created by Marcin Rainka on 31/07/16.
+//  Copyright © 2016 Marcin Rainka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iQuizek/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iQuizek
+//
+//  Created by Marcin Rainka on 31/07/16.
+//  Copyright © 2016 Marcin Rainka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
